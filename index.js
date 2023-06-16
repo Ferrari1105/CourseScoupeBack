e1094aa9d2474870d62fc8a1d582219abe9238cb
Rocco Ferrari <roccoferrari5@gmail.com>
2023-06-16 11:14:24 -0300

    conectando con front

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
 import express from "express";
 import Usuario_Services from './src/services/usuario-services.js'
-import Usuario from './src/models/usuario.js'
 import cors from 'cors'
 
-const svcUsuario=new Usuario_Services();
+const svcUsuario = new Usuario_Services();
 
 const app= express();
-const port= 3001;
-
+const port= 3000;
+app.use(cors())
+app.use(express.json())
 app.get('/', (req,res)=>{
-  
+    
     res.send('CourseScope');
-  
+    
 })
 
 app.listen(port,()=> {
     console.log(`Pagina abierte en el puerto ${port}`)
 })
-
-app.get('/usuarios',async (req,res) =>{
-    const UsuariosGetAll = await svcUsuario.getAll();
-    return res.status(200).json(UsuariosGetAll)
-})
-app.use(cors())
+async function getByName(nombre){
+    let data;
+    data = await svcUsuario.getByName(nombre)
+    console.log(data);
+}
+app.get('/usuariosXNombre',async (req,res) =>{
+    let nombre = req.query.usuario
+    console.log(nombre)
+    const UsuarioPorNombre = await getByName(nombre);
+    return res.status(200).json(UsuarioPorNombre)
+})  
--- src/services/usuario-services.js
+++ src/services/usuario-services.js
@@ -2,12 +2,12 @@ import config from '../../dbconfig-env.js';
 import sql from 'mssql'
 class Usuario_Services{
     
-getAll = async () => {
+getAllUsuarios = async () => {
     let returnArray = null;
     console.log('Estoy en UsuarioServices.getAll()');
     try {
         let pool = await sql.connect(config);
-       let result = await pool.request().query("SELECT * FROM Usuario");
+       let result = await pool.request().query("SELECT * FROM Usuario WHERE ");
        returnArray = result.recordsets[0];
     }
     catch (error){
@@ -15,6 +15,35 @@ getAll = async () => {
     }
     return returnArray;
     }
+    getByName = async (nombre) => {
+        let returnEntity = null;
+        console.log('Estoy en: GetByName');
+        try {
+            let pool = await sql.connect(config);
+            let result = await pool.request()
+            .input('pNombre', sql.Text, nombre)
+            .query('SELECT * FROM Usuario WHERE NombreUsuario = @pNombre');
+            returnEntity = result.recordsets[0][0];
+        } catch (error) {
+            console.log(error);
+        }
+        return returnEntity;
+    }
+    
+    insertUsuario = async (Usuario) => {
+        let rowsAffected = 0;
+        console.log('Estoy en: insertUsuario');
+        try {
+            let pool = await sql.connect(config);
+            let result = await pool.request()
+            .query(`INSERT INTO Usuario (NombreUsuario, Contraseña, Email, TipoUsuario)
+            VALUES ('${Usuario.NombreUsuario}', '${Usuario.Contraseña}', '${Usuario.Email}', '${Usuario.TipoUsuario}')`);
+            rowsAffected = result.rowsAffected;
+        } catch (error) {
+            console.log(error);
+        }
+        return rowsAffected;
+    }
 }
-
+    
 export default Usuario_Services;
